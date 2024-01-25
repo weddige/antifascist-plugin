26319e62dcf7b68df0bc6828cfb124f166f29707
Konstantin Weddige <konstantin.weddige@lutrasecurity.com>
2024-01-25 23:57:34 +0100

    Add German translation

--- antifascist_registration.php
+++ antifascist_registration.php
@@ -7,6 +7,7 @@ Version: 1.0.0
 Author: Konstantin Weddige
 Author URI: https://weddige.eu
 License: MIT
+Text Domain: antifascist-registration
 */
 
 // If this file is called directly, abort.
@@ -16,13 +17,21 @@ if (!defined('WPINC')) {
 
 define('ANTIFASCIST_REGISTRATION_VERSION', '1.0.0');
 
+
+function antifascist_register_load_textdomain()
+{
+    load_plugin_textdomain('antifascist-registration', false, dirname(plugin_basename(__FILE__)));
+}
+
+add_action('init', 'antifascist_register_load_textdomain');
+
 function antifascist_register_form()
 {
     echo '<p>
-        <label for="fascism_status" style="width:100%">' . __('I am') . '
+        <label for="fascism_status" style="width:100%">' . __('I am', 'antifascist-registration') . '
             <select name="fascism_status" id="fascism_status" style="width:100%">
-                <option value="antifascist" selected>' . __('an antifascist') . '</option>
-                <option value="fascist">' . __('a fascist') . '</option>
+                <option value="antifascist" selected>' . __('an antifascist', 'antifascist-registration') . '</option>
+                <option value="fascist">' . __('a fascist', 'antifascist-registration') . '</option>
             </select>
         </label>
     </p>';
@@ -31,7 +40,7 @@ function antifascist_register_form()
 function antifascist_register_post($login, $email, $errors)
 {
     if ($_POST['fascism_status'] != 'antifascist') {
-        $errors->add('antifascist_plugin_error', __('Hate is not an opinion.'));
+        $errors->add('antifascist_plugin_error', __('Hate is not an opinion.', 'antifascist-registration'));
     }
 }
 
